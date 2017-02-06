cb855fc2f413432f4e8a5870e10f402f0a572c5b
unknown <loretaanyanje@gmail.com>
2017-02-06 21:56:08 +0300

    2nd commit

--- primenumber.py
+++ primenumber.py
@@ -1,4 +1,10 @@
+# python prime number program 
 def isprimeno(num):
-    """A function that returns all prime numbers in a certain range"""
-	pass
-	
+ #A function that returns all prime numbers in a certain range"""
+ num=n
+ 
+ 
+ #user inputs a number
+ #num=input("enter number:")
+ 
+ 
