7ab0a6b63eb905e369aa7e0d347e4cf486e7ecbf
unknown <loretaanyanje@gmail.com>
2017-02-06 22:17:10 +0300

    fourth commit

--- primenumber.py
+++ primenumber.py
@@ -3,13 +3,18 @@ def isprimeno(num):
  #A function that returns all prime numbers in a certain range"""
  num=n
  
- 
  #user inputs a number
  #num=input("enter number:")
  
  #we know that prime numbers are greater than one
  if num > =1:
+ #check factors all the time 
  for i in range(2,num):
  if (num % i)==0:
  print (num,"not a prime"):
+ break
+ else:
+ # Give the out put
+ print(num,"is prime"):
+ 
  
