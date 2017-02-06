7559db04f3c679cc9def5918ed3fda16ff54e172
unknown <loretaanyanje@gmail.com>
2017-02-06 22:10:02 +0300

    third commit

--- primenumber.py
+++ primenumber.py
@@ -7,4 +7,9 @@ def isprimeno(num):
  #user inputs a number
  #num=input("enter number:")
  
+ #we know that prime numbers are greater than one
+ if num > =1:
+ for i in range(2,num):
+ if (num % i)==0:
+ print (num,"not a prime"):
  
